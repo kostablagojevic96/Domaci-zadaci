05537933a74b3cd964bab07746029a6cf4e8d5de
kostablagojevic96 <blagojevickostaa@gmail.com>
2023-04-21 11:59:08 +0200

    Ispravljen 5 domaci

--- D_5_p11nw23/index1.php
+++ D_5_p11nw23/index1.php
@@ -1,19 +1,53 @@
-<?php //Zadatak br. 1
-/*Izvršiti konverziju temperature iz Farenhajta u Kelvine i obratno, ukoliko su date sledeće dve formule:
-
-Celzijus = (Farenhajt - 32) * 5/9
-Kelvin = Celzijus + 273.15
-
-Temperaturu u Farenhajtima i Kelvinima zadati samostalno.
- */
+<?php 
+//Zadatak br. 1
+echo "Zadatak br.1";
+echo "<br>";
 $Farenhajt = 104;
 $Celzijus = ($Farenhajt -32) * 5 / 9;
 $Kelvin = $Celzijus + 273.15;
-$JedanKelvin = $Kelvin / $Farenhajt;
-$JedanFarenhajt = $Farenhajt / $Kelvin; 
+echo "$Kelvin";
 echo "<br>";
-echo "1K= " . " $JedanKelvin" . " Farenhajta";
+$Kelvin = 300;
+$Celzijus = $Kelvin - 273.15;
+$Farenhajt = $Celzijus * 9 / 5 + 32;
+echo "$Farenhajt";
 echo "<br>";
-echo "1F= " . "$JedanFarenhajt" . " Kelvina";
+echo "<hr>";
+//Zadatak br. 2
+echo "Zadatak br.2";
+echo "<br>";
+$n = 110;
+$a = 7;
+$drugiDan = $a + 2;
+$ostaloPoglavlja = $n - ($a + $drugiDan);
+echo "Ostalo je da procita jos " . $ostaloPoglavlja . " poglavlja.";
+echo "<br>";
+echo "<hr>";
+//Zadatak br.3
+echo "Zadatak br.3";
+echo "<br>";
+$p = 800;
+$m = 700;
+$k = 200;
+$Dzemper = ($p + $m - $k) / 2;
+$PerinKusur = $p - $Dzemper;
+$MikinKusur = $m - $Dzemper;
+echo "Perin kusur je " . $PerinKusur . " dinara";
+echo "<br>";
+echo " Mikin kusur je " . $MikinKusur . " dinara";
+echo "<br>";
+echo "Da bi jednako platili svako svoj dzemper Perin i Mikin kusur treba da je isti ili jednak 0, ili da obojica dobiju kusur u nekom iznosu.";
+echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
